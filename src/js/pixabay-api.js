9bd44f245d75352a6caba0f8eb2288f1c43ae298
InnaIvBoiko <inna_boiko@libero.it>
2024-02-21 19:17:37 +0100

    Update the structure and js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,37 +3,21 @@ import "izitoast/dist/css/iziToast.min.css";
 
 import ErrorIcon from '../img/error.svg';
 
-
-const formSearch = document.querySelector('.form');
-const keywords = document.querySelector('.key-words');
+const keywords = document.querySelector('.keywords');
 
 const API_KEY = '42438077-634a4b32cfcaa96ebaa8c4719';
+const API_URL = 'https://pixabay.com/api/';
 
 async function searchImg() {
-    const keywordsValue = encodeURIComponent(keywords.value);  
-    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${keywordsValue}`;
-
-    const response = await fetch(url);
+    const QUERY = encodeURIComponent(keywords.value);
+    const URL = `${API_URL}?key=${API_KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&safesearch=true`;
+    
+    const response = await fetch(URL);
     const data = await response.json();
-
-    if (parseInt(data.totalHits) > 0) {
-        return data;
-    } else {
-        errorMessage();
-    };   
+    return data;
 };
 
-formSearch.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    if (keywords.value) {
-        searchImg();
-    };
-
-    formSearch.reset();
-});
-
-const errorMessage = () => {
+const errorMessage = () =>
     iziToast.show({
         message: 'Sorry, there are no images matching your search query. Please try again!',
         messageColor: '#fff',
@@ -43,8 +27,7 @@ const errorMessage = () => {
         position: 'topRight',
         close: true,
         layout: 2,
-        timeout: 3000
+        timeout: 2000
     });
-};
 
-
+export { keywords, searchImg, errorMessage };
